5050713af5a8f0da885d72e393ea4d5e910e8e05
osee <olindbin@gmail.com>
2026-01-31 17:32:36 +0600

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
